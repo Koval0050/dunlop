17d7be0a3da9f09e80bd7be2ca8749d1e82896cd
Koval0050 <121100028+Koval0050@users.noreply.github.com>
2024-01-25 19:44:16 +0200

    add swiper

--- static/development/components/pages/product/swiper.js
+++ static/development/components/pages/product/swiper.js
@@ -1,2 +1,27 @@
 import Swiper, { Autoplay } from "swiper";
 import "swiper/swiper-bundle.min.css";
+
+export const reviewsSwiper = new Swiper(".reviews-swiper", {
+  modules: [Autoplay],
+  slidesPerView: 3,
+  spaceBetween: 15,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
+  //   breakpoints: {
+  //     0: {
+  //       slidesPerView: 1,
+  //     },
+
+  //     830: {
+  //       slidesPerView: 2,
+  //     },
+
+  //     1150: {
+  //       slidesPerView: 3,
+  //     },
+  //   },
+});
